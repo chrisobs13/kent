f8f4bdeb362521231b36d77fbeee74d36c562fbb
Chris OBrien <chris@chrisobriendesign.com>
2013-09-04 10:19:19 -0400

    add new images

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
     <div id="content">
 		<div id="photos">
             <img src="images/kent-slide-1.jpg" alt="custom cape cod home by JM Kent Building"/>
-            <img src="images/kent-slide-2.jpg" alt="custom cape cod home by JM Kent Building"/>
-            <img src="images/kent-slide-3.jpg" alt="custom cape cod home by JM Kent Building"/>
+            <img src="images/kent-slide-2.jpg" alt="custom living room by JM Kent Building"/>
+            <img src="images/kent-slide-3.jpg" alt="living room with fireplace and exposed beams by JM Kent Building"/>
+            <img src="images/kent-slide-4.jpg" alt="custom cape cod home by JM Kent Building"/>
+            <img src="images/kent-slide-5.jpg" alt="outdoor pool on cape cod at night"/>
+
 		</div>
     </div>
         
